0f24badb4a405185b1aaafc061a78110fc9ee3a2
AKHIL A <56191225+akhil131@users.noreply.github.com>
2022-07-13 22:46:27 +0530

    Create resetPassword.dart

---
+++ resetPassword.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class resetPassword extends StatefulWidget {
+  const resetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<resetPassword> createState() => _resetPasswordState();
+}
+
+class _resetPasswordState extends State<resetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
